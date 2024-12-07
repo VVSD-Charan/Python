5139a605e23032213da33f8e088465e648a59fee
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2024-12-07 17:20:55 +0530

    Added importing and exporting packages notebooks

---
+++ Modules and packages/Packages/Subpackages/.ipynb_checkpoints/custom-checkpoint.py
@@ -0,0 +1,8 @@
+def square(number):
+    return number**2
+
+def cube(number):
+    return number**3
+
+def quadraple(number):
+    return number**4
---
+++ Modules and packages/Packages/__init__.py
@@ -0,0 +1,1 @@
+# To create custom packages, we need __init__.py file. Even if this file is empty there is no issue but without this file, packages will not be traceable
